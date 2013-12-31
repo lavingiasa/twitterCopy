438705904b2f6a5a9a4ba7639d14dbe844c4a64f
Samir Lavingia <lavingiasa@gmail.com>
2013-12-31 17:21:01 -0300

    Made my tests more DRY by not repeating where I don't have to. Part of the title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  let(:mainTitle) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +11,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{mainTitle} | Home")
     end
   end
 
@@ -24,7 +24,7 @@ describe "Static pages" do
 
      it "should have the title 'Help'" do
        visit '/static_pages/help'
-       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+       expect(page).to have_title("#{mainTitle} | Help")
      end
    end
 
@@ -38,7 +38,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
        visit '/static_pages/about'
-       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+       expect(page).to have_title("#{mainTitle} | About Us")
     end
   end
 
@@ -50,7 +50,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+      expect(page).to have_title("#{mainTitle} | Contact Us")
     end
   end
 
