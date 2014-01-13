b30d6e7a0685a904bef3e65120ee88f6e246277c
Samir Lavingia <lavingiasa@gmail.com>
2014-01-13 13:50:13 -0800

    Started the security stuff to make sure users cannot edit other users. At 9.2.3 now

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,10 @@ module SessionsHelper
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
-  end  
+  end 
+  
+  def current_user?(user)
+    user == current_user
+  end 
 
 end
