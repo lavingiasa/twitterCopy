9f5a4137767ef348a045de8101390b9f87fca02b
Samir Lavingia <lavingiasa@gmail.com>
2014-01-23 20:46:13 -0800

    Pagination now works :D

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only:[:edit, :update]
+  before_action :signed_in_user, only:[:index,:edit, :update]
   before_action :correct_user,   only:[:edit, :update]  
   def show
     @user = User.find(params[:id])
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def update
