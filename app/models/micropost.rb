3c6cdde2f503d947bf68e60259ff6e80c4bc256a
Samir Lavingia <lavingiasa@gmail.com>
2014-02-03 19:16:02 -0800

    Made it show microposts and stuff like that

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ActiveRecord::Base
   validates :user_id, presence:true
   default_scope -> {order('created_at DESC')}
-  validates(:content, length: {maximum: 140})
-  belongs_to :user 
+  validates(:content, length: {maximum: 140}, presence: true)
+  belongs_to :user
 end
